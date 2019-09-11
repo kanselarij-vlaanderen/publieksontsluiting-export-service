b3d089690dca5c59a7ef29ce7ed554aae0cc7698
Claire Lovisa <lovisa.claire@gmail.com>
2019-09-11 17:08:36 +0200

    Add queries to fill the export graph

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import { app, query, errorHandler } from 'mu';
 import { writeToFile } from './lib/graph-helpers';
 import { queryKaleidos } from './lib/kaleidos';
 import { copyToLocalGraph } from './lib/query-helpers';
+import {
+  parseResult,
+  getMeetingUriFromKaleidos,
+  constructMeetingInfo,
+  constructProcedurestapInfo,
+  getProcedurestappenInfoFromTmp,
+  constructNieuwsbriefInfo
+} from './queries';
 
 app.get('/', function( req, res ) {
   res.send('Hello from publieksontsluiting-export-service');
@@ -9,12 +17,39 @@ app.get('/', function( req, res ) {
 
 app.post('/export/:uuid', async function( req, res ) {
   const timestamp = new Date().toISOString().replace(/\D/g,'').substring(0, 14);
-  const file = `/data/exports/${timestamp}-publieksontsluiting.ttl`;
-  await writeToFile('http://mu.semte.ch/application', file);
+  const tmpGraph = `http://mu.semte.ch/graphs/tmp/${timestamp}`;
+  const kaleidosGraph = `http://mu.semte.ch/graphs/organizations/kanselarij`;
+  const exportGraph = `http://mu.semte.ch/graphs/export/${timestamp}`;
+  const uuid = req.params.uuid;
 
-  res.status(200).send({
-    export: file
-  });
+  const result = await getMeetingUriFromKaleidos(kaleidosGraph, uuid);
+  try {
+    const meetingUri = parseResult(result)[0].s;
+
+    const meetingInfo = constructMeetingInfo(kaleidosGraph, uuid)
+    await copyToLocalGraph(meetingInfo, exportGraph);
+
+    const procedurestapInfo = constructProcedurestapInfo(kaleidosGraph, meetingUri)
+    await copyToLocalGraph(procedurestapInfo, tmpGraph);
+
+    const resultProcedurestappenInfo = await getProcedurestappenInfoFromTmp(tmpGraph);
+    const procedurestappenInfo = parseResult(resultProcedurestappenInfo);
+
+    procedurestappenInfo.forEach(procedurestapInfo => {
+      const nieuwsbriefInfo = constructNieuwsbriefInfo(kaleidosGraph, procedurestapInfo)
+      copyToLocalGraph(nieuwsbriefInfo, exportGraph);
+    });
+
+    const file = `/data/exports/${timestamp}-publieksontsluiting.ttl`;
+    await writeToFile(graph, file);
+
+    res.status(200).send({
+      // export: file
+    });
+  } catch(e) {
+    console.log(`No ziting found for this uuid: ${uuid}`);
+    res.status(204).send({'msg': `No ziting found for this uuid: ${uuid}`});
+  }
 });
 
 // TODO remove this dummy function
@@ -36,5 +71,7 @@ app.get('/copy-kaleidos', async function( req, res ) {
   await copyToLocalGraph(constructQuery, 'http://mu.semte.ch/graphs/copy-kaleidos');
   res.status(204).send({});
 } );
+
+
 
 app.use(errorHandler);
---
+++ queries.js
@@ -0,0 +1,132 @@
+import { query, sparqlEscapeString, sparqlEscapeUri } from 'mu';
+import { queryKaleidos } from './lib/kaleidos';
+
+/**
+ * Convert results of select query to an array of objects.
+ * @method parseResult
+ * @return {Array}
+ */
+function parseResult(result) {
+  const bindingKeys = result.head.vars;
+  return result.results.bindings.map((row) => {
+    const obj = {};
+    bindingKeys.forEach((key) => obj[key] = row[key].value);
+    return obj;
+  });
+};
+
+async function getMeetingUriFromKaleidos(kaleidosGraph, uuid) {
+  return await queryKaleidos(`
+    SELECT ?s
+    WHERE {
+      GRAPH ${sparqlEscapeUri(kaleidosGraph)} {
+        ?s a <http://data.vlaanderen.be/ns/besluit#Zitting> ;
+          <http://mu.semte.ch/vocabularies/core/uuid> ${sparqlEscapeString(uuid)} .
+      }
+    }
+  `);
+}
+
+function constructMeetingInfo(kaleidosGraph, uuid) {
+  return `
+    PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+
+    CONSTRUCT {
+      ?s a besluit:Zitting ;
+        mu:uuid ${sparqlEscapeString(uuid)} ;
+        besluit:geplandeStart ?geplandeStart .
+    }
+    WHERE {
+      GRAPH ${sparqlEscapeUri(kaleidosGraph)} {
+        ?s a besluit:Zitting ;
+          mu:uuid ${sparqlEscapeString(uuid)} ;
+          besluit:geplandeStart ?geplandeStart .
+      }
+    }
+  `;
+}
+
+function constructProcedurestapInfo(kaleidosGraph, meetingUri) {
+  return `
+  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+  PREFIX dbpedia: <http://dbpedia.org/ontology/>
+  PREFIX dct: <http://purl.org/dc/terms/>
+  PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
+
+  CONSTRUCT {
+    ?s a dbpedia:UnitOfWork ;
+      mu:uuid ?uuid ;
+      besluitvorming:heeftBevoegde ?heeftBevoegde .
+  }
+  WHERE {
+    GRAPH ${sparqlEscapeUri(kaleidosGraph)} {
+      ${sparqlEscapeUri(meetingUri)} besluitvorming:behandelt ?agenda .
+      ?agenda dct:hasPart ?agendapunt .
+      ?s a dbpedia:UnitOfWork ;
+        besluitvorming:isGeagendeerdVia ?agendapunt ;
+        mu:uuid ?uuid ;
+        besluitvorming:heeftBevoegde ?heeftBevoegde .
+    }
+  }`;
+}
+
+async function getProcedurestappenInfoFromTmp(tmpGraph) {
+  return await query(`
+    PREFIX dbpedia: <http://dbpedia.org/ontology/>
+    PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
+
+    SELECT ?s ?heeftBevoegde
+    WHERE {
+      GRAPH ${sparqlEscapeUri(tmpGraph)} {
+        ?s a dbpedia:UnitOfWork ;
+          besluitvorming:heeftBevoegde ?heeftBevoegde .
+      }
+    }
+  `);
+}
+
+function constructNieuwsbriefInfo(kaleidosGraph, procedurestapInfo) {
+  return `
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX prov: <http://www.w3.org/ns/prov#>
+
+    CONSTRUCT {
+      ?s a besluitvorming:NieuwsbriefInfo ;
+        mu:uuid ?uuid ;
+        dct:title ?title ;
+        ext:htmlInhoud ?htmlInhoud ;
+        ext:themesOfSubcase ?themesOfSubcase .
+      ${sparqlEscapeUri(procedurestapInfo.s)} besluitvorming:heeftBevoegde ?heeftBevoegde ;
+        prov:generated ?s ;
+        besluitvorming:isGeagendeerdVia ?agendapunt .
+      ?agendapunt ext:prioriteit ?priorty .
+    }
+    WHERE {
+      GRAPH ${sparqlEscapeUri(kaleidosGraph)} {
+        ?s a besluitvorming:NieuwsbriefInfo ;
+          ext:afgewerkt \"true\"^^<http://mu.semte.ch/vocabularies/typed-literals/boolean> ;
+          ^prov:generated ${sparqlEscapeUri(procedurestapInfo.s)} ;
+          mu:uuid ?uuid ;
+          dct:title ?title ;
+          ext:htmlInhoud ?htmlInhoud ;
+          ext:themesOfSubcase ?themesOfSubcase .
+        ${sparqlEscapeUri(procedurestapInfo.s)} besluitvorming:heeftBevoegde ?heeftBevoegde ;
+          besluitvorming:isGeagendeerdVia ?agendapunt .
+        ?agendapunt ext:prioriteit ?priorty .
+      }
+    }
+  `;
+}
+
+export {
+  parseResult,
+  getMeetingUriFromKaleidos,
+  constructMeetingInfo,
+  constructProcedurestapInfo,
+  getProcedurestappenInfoFromTmp,
+  constructNieuwsbriefInfo
+}
